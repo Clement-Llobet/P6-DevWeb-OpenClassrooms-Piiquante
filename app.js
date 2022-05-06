97a780a88efe81c78537e645c5a3d30b94e1d1f6
Clément Llobet <clement.llobet@tutanota.com>
2022-05-06 10:32:49 +0200

    delete useless comments

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use((req, res, next) => {
 app.use(helmet({crossOriginResourcePolicy: false}));
 
 const apiLimiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+	standardHeaders: true,
+	legacyHeaders: false,
 })
 
 app.use('/images', express.static(path.join(__dirname, 'images')));

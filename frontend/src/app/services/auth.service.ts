c508cd509b7560fd01324bb02ebef93c87ccfe84
Clément Llobet <clement.llobet@tutanota.com>
2022-07-13 10:03:55 +0200

    update frontend services links with heroku app link

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -4,19 +4,20 @@ import { BehaviorSubject, tap } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   isAuth$ = new BehaviorSubject<boolean>(false);
   private authToken = '';
   private userId = '';
 
-  constructor(private http: HttpClient,
-              private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   createUser(email: string, password: string) {
-    return this.http.post<{ message: string }>('http://localhost:3000/api/auth/signup', {email: email, password: password});
+    return this.http.post<{ message: string }>(
+      'https://p6-clement-llobet-oc.herokuapp.com/api/auth/signup',
+      { email: email, password: password }
+    );
   }
 
   getToken() {
@@ -28,13 +29,18 @@ export class AuthService {
   }
 
   loginUser(email: string, password: string) {
-    return this.http.post<{ userId: string, token: string }>('http://localhost:3000/api/auth/login', {email: email, password: password}).pipe(
-      tap(({ userId, token }) => {
-        this.userId = userId;
-        this.authToken = token;
-        this.isAuth$.next(true);
-      })
-    );
+    return this.http
+      .post<{ userId: string; token: string }>(
+        'https://p6-clement-llobet-oc.herokuapp.com/api/auth/login',
+        { email: email, password: password }
+      )
+      .pipe(
+        tap(({ userId, token }) => {
+          this.userId = userId;
+          this.authToken = token;
+          this.isAuth$.next(true);
+        })
+      );
   }
 
   logout() {
@@ -43,5 +49,4 @@ export class AuthService {
     this.isAuth$.next(false);
     this.router.navigate(['login']);
   }
-
 }
--- frontend/src/app/services/sauces.service.ts
+++ frontend/src/app/services/sauces.service.ts
@@ -5,78 +5,94 @@ import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SaucesService {
-
   sauces$ = new Subject<Sauce[]>();
 
-  constructor(private http: HttpClient,
-              private auth: AuthService) {}
+  constructor(private http: HttpClient, private auth: AuthService) {}
 
   getSauces() {
-    this.http.get<Sauce[]>('http://localhost:3000/api/sauces').pipe(
-      tap(sauces => this.sauces$.next(sauces)),
-      catchError(error => {
-        console.error(error.error.message);
-        return of([]);
-      })
-    ).subscribe();
+    this.http
+      .get<Sauce[]>('https://p6-clement-llobet-oc.herokuapp.com/api/sauces')
+      .pipe(
+        tap((sauces) => this.sauces$.next(sauces)),
+        catchError((error) => {
+          console.error(error.error.message);
+          return of([]);
+        })
+      )
+      .subscribe();
   }
 
   getSauceById(id: string) {
-    return this.http.get<Sauce>('http://localhost:3000/api/sauces/' + id).pipe(
-      catchError(error => throwError(error.error.message))
-    );
+    return this.http
+      .get<Sauce>('https://p6-clement-llobet-oc.herokuapp.com/api/sauces/' + id)
+      .pipe(catchError((error) => throwError(error.error.message)));
   }
 
   likeSauce(id: string, like: boolean) {
-    return this.http.post<{ message: string }>(
-      'http://localhost:3000/api/sauces/' + id + '/like',
-      { userId: this.auth.getUserId(), like: like ? 1 : 0 }
-    ).pipe(
-      mapTo(like),
-      catchError(error => throwError(error.error.message))
-    );
+    return this.http
+      .post<{ message: string }>(
+        'https://p6-clement-llobet-oc.herokuapp.com/api/sauces/' + id + '/like',
+        { userId: this.auth.getUserId(), like: like ? 1 : 0 }
+      )
+      .pipe(
+        mapTo(like),
+        catchError((error) => throwError(error.error.message))
+      );
   }
 
   dislikeSauce(id: string, dislike: boolean) {
-    return this.http.post<{ message: string }>(
-      'http://localhost:3000/api/sauces/' + id + '/like',
-      { userId: this.auth.getUserId(), like: dislike ? -1 : 0 }
-    ).pipe(
-      mapTo(dislike),
-      catchError(error => throwError(error.error.message))
-    );
+    return this.http
+      .post<{ message: string }>(
+        'https://p6-clement-llobet-oc.herokuapp.com/api/sauces/' + id + '/like',
+        { userId: this.auth.getUserId(), like: dislike ? -1 : 0 }
+      )
+      .pipe(
+        mapTo(dislike),
+        catchError((error) => throwError(error.error.message))
+      );
   }
 
   createSauce(sauce: Sauce, image: File) {
     const formData = new FormData();
     formData.append('sauce', JSON.stringify(sauce));
     formData.append('image', image);
-    return this.http.post<{ message: string }>('http://localhost:3000/api/sauces', formData).pipe(
-      catchError(error => throwError(error.error.message))
-    );
+    return this.http
+      .post<{ message: string }>(
+        'https://p6-clement-llobet-oc.herokuapp.com/api/sauces',
+        formData
+      )
+      .pipe(catchError((error) => throwError(error.error.message)));
   }
 
   modifySauce(id: string, sauce: Sauce, image: string | File) {
     if (typeof image === 'string') {
-      return this.http.put<{ message: string }>('http://localhost:3000/api/sauces/' + id, sauce).pipe(
-        catchError(error => throwError(error.error.message))
-      );
+      return this.http
+        .put<{ message: string }>(
+          'https://p6-clement-llobet-oc.herokuapp.com/api/sauces/' + id,
+          sauce
+        )
+        .pipe(catchError((error) => throwError(error.error.message)));
     } else {
       const formData = new FormData();
       formData.append('sauce', JSON.stringify(sauce));
       formData.append('image', image);
-      return this.http.put<{ message: string }>('http://localhost:3000/api/sauces/' + id, formData).pipe(
-        catchError(error => throwError(error.error.message))
-      );
+      return this.http
+        .put<{ message: string }>(
+          'https://p6-clement-llobet-oc.herokuapp.com/api/sauces/' + id,
+          formData
+        )
+        .pipe(catchError((error) => throwError(error.error.message)));
     }
   }
 
   deleteSauce(id: string) {
-    return this.http.delete<{ message: string }>('http://localhost:3000/api/sauces/' + id).pipe(
-      catchError(error => throwError(error.error.message))
-    );
+    return this.http
+      .delete<{ message: string }>(
+        'https://p6-clement-llobet-oc.herokuapp.com/api/sauces/' + id
+      )
+      .pipe(catchError((error) => throwError(error.error.message)));
   }
 }
